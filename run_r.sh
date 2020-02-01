c2e755ab68e8fad4ef0d3747a192eaeacd13869c
alan <alanrgln@gmail.com>
2020-02-01 00:26:21 -0200

    ajuste script

--- run_r.sh
+++ run_r.sh
@@ -4,5 +4,4 @@ NAME=rstudio
 docker build -t $NAME:1.0 .
 docker stop $NAME || true
 docker rm -f $NAME || true
-#docker run -d --name $NAME --restart always -e TZ=America/Sao_Paulo -p 9999:9999 $NAME:1.0
 docker run -d --name $NAME --restart always -e PASSWORD=ds_sudad -p 8787:8787 $NAME:1.0
