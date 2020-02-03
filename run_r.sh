04e0d0ac833e3e36f1181695d5f5eda9b5594d33
Alan <alanrgln@gmail.com>
2020-02-03 18:37:09 -0300

    Update run_r.sh

--- run_r.sh
+++ run_r.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 NAME=rstudio
 
-docker build -t $NAME:1.0 .
+#docker build -t $NAME:1.0 .
+docker load < rstudio.tar.gz
 docker stop $NAME || true
 docker rm -f $NAME || true
-docker run -d --name $NAME --restart always -e PASSWORD=ds_sudad -p 8787:8787 $NAME:1.0
+docker run -d --name $NAME --restart always -e PASSWORD=ds_sudad -p 8787:8787 $NAME:1.1
 #docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
